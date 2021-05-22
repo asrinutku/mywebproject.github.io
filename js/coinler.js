5fd5c75c46d609ced771ec31ff613fac40c690d4
asrinutku <auyagmur@hotmail.com>
2021-05-22 15:58:37 +0300

    json fetch example

---
+++ js/coinler.js
@@ -0,0 +1,37 @@
+
+function fetchData(){
+    
+    fetch('https://jsonplaceholder.typicode.com/todos').then(res =>{
+        
+        if(!res.ok){
+            throw Error("ERROR");
+        }
+
+        return res.json();
+        
+    })
+    
+    .then(data=>{
+        const html = data.map(user =>{
+            return  `
+            <div class="user">
+                <p>UserId: ${user.userId}</p>
+                <p>Id: ${user.id}</p>
+                <p>Title: ${user.title}</p>
+                <p>Completed: ${user.completed}</p>
+                <br>
+
+            </div>
+            `
+        }).join("");
+
+      
+        document.querySelector('#app').insertAdjacentHTML("afterbegin",html);
+    })
+    
+    .catch(err=>{
+        console.log(err);
+    })
+}
+
+fetchData();
